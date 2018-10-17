fa65f58e717470487ce74724e253571487219073
Ricardo Mayo <rmayo.1995@gmail.com>
2018-10-16 21:25:24 -0500

    mailer

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router  = express.Router();
+const mailer = require('../helpers/mailer');
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.post('/', (req, res, next) => {
+  let options = req.body;
+  options.filename = 'verify';
+  mailer.send(options)
+  .then(() => {
+    res.status(200).send('El correo ya llegó, anunciando su canción');
+  })
+  .catch(err => {
+    console.log('Algo salió mal', err);
+    res.status(500).json({err, 'msg': 'Algo salió mal'});
+  })
+});
+
+module.exports = router;
